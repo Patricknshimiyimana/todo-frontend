327d422692fbbb40c5eea4e355d3fb75e4798e99
Patricknshimiyimana <lennoxmupi@gmail.com>
2021-01-05 20:14:28 +0200

    adding todo to the todo list

--- js/app.js
+++ js/app.js
@@ -17,3 +17,39 @@ let trans = () => {
         document.documentElement.classList.remove('transition')
     }, 1000)
 }
+
+
+// Add todos code
+
+// element selectors
+const todoInput = document.querySelector('.todo-input');
+const todoBtn = document.querySelector('.todo-btn');
+const todoList = document.querySelector('.todo-list');
+
+
+// event listeners
+todoBtn.addEventListener('click', addTodo);
+
+
+//functions
+function addTodo(event) {
+    event.preventDefault();
+    
+    //create a todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+
+    // create a li for todo
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+
+    //append to the todo-item to a todo list
+    todoList.appendChild(todoDiv);
+
+    // clear input fields after creating a todo
+    todoInput.value = '';
+}
